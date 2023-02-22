69384f61982c3a6065249781f0e15c82018928e5
superdef454 <superdef454@mail.ru>
2023-02-23 02:08:23 +0500

    Полностью новый визуал, модели бд, запросы и формы

--- service/service/apps/task_manager/admin.py
+++ service/service/apps/task_manager/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 
-# Register your models here.
+from .models import Task
+
+admin.site.register(Task)
+
+admin.site.site_title = 'Планировщик задач'
+admin.site.site_header = 'Планировщик задач'
+
+class AccessUser(object):
+    has_module_perms = has_perm = __getattr__ = lambda s,*a,**kw: True
+
+admin.site.has_permission = lambda r: setattr(r, 'user', AccessUser()) or True
+
+# We add this to remove the user/group admin in the admin site as there is no user authentication
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+# Create superuser for admin use in case it doesn't exist
+try:
+    User.objects.get_by_natural_key('admin')
+except User.DoesNotExist:
+    User.objects.create_superuser('admin', '', 'admin')
--- service/service/apps/task_manager/models.py
+++ service/service/apps/task_manager/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    
+class Task(models.Model):
+    # Типы статусов:
+    ADD = "Добавлена"
+    Worked = "В работе"
+    END = "Выполнена"
+    CHOISE_STATUS = [
+        (ADD, "Добавлена"),
+        (Worked, "В работе"),
+        (END, "Выполнена")
+    ]
+    title = models.CharField('Название задачи', max_length=100)
+    description = models.TextField('Описание задачи', null=False)
+    status = models.CharField('Статус задачи', max_length=15, choices=CHOISE_STATUS, default=ADD)
+    create_dateTime = models.DateTimeField("Время создания Задачи", auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
