8650fcfec56448dd277e00c5b73a37985b26c279
superdef454 <superdef454@mail.ru>
2023-02-23 21:05:20 +0500

    modified:   service/db.sqlite3 	modified:   service/service/apps/task_manager/views.py

--- service/service/apps/task_manager/views.py
+++ service/service/apps/task_manager/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+import pytz
+
 
 from .models import Task
 
 def tasks(request, id: int = -1, status: str = None):
+    # Разбор изменения статуса задачи
     if id >= 0 and status:
         if Task.objects.filter(id=id).exists():
             task = Task.objects.get(id=id)
@@ -11,11 +15,27 @@ def tasks(request, id: int = -1, status: str = None):
                 task.save()
             if status == "Удалить":
                 task.delete()
+    # Добавление новой и проверка на наличие добовляемой записи в БД
     if request.GET.get("Title") and request.GET.get("description") and request.GET.get("status"):
         if not Task.objects.filter(title=request.GET.get("Title")).exists():
             Task.objects.create(title=request.GET.get("Title"), description = request.GET.get("description"), status = request.GET.get("status"))
-    Tasks = Task.objects.all()
+    # Я понимаю, что часть функционала нужно разнести по разным функциям и путям, но время мало
     out = {"Задачи" : []}
+    # Для корректного определения времени
+    current_tz = pytz.timezone('Asia/Yekaterinburg')
+    timezone.activate(current_tz)
+    # Была попытка объединить 3 запроса в один
+    # Tasks = Task.objects.filter(status=Task.WORKED).union(Task.objects.filter(status=Task.ADD)).union(Task.objects.filter(status=Task.END))
+    # или так
+    # from django.db.models import Q
+    # Tasks = Task.objects.filter(Q(status=Task.WORKED) and Q(status=Task.ADD) and Q(status=Task.END))
+    # или так
+    # work = Task.objects.filter(status=Task.WORKED)
+    # add = Task.objects.filter(status=Task.ADD)
+    # end = Task.objects.filter(status=Task.END)
+    # Tasks = work | add | end 
+    # Но все не то, поэтому поочерёдные запросы
+    Tasks = Task.objects.filter(status=Task.WORKED)
     for i in Tasks:
         out["Задачи"].append({
             "Имя" : i.title,
@@ -24,11 +44,16 @@ def tasks(request, id: int = -1, status: str = None):
             "id": i.id,
             "Время" : i.create_dateTime,
         })
-    return render(request, 'index.html', out)
-
-def changeStatus(request):
-    Tasks = Task.objects.all()
-    out = {"Задачи" : []}
+    Tasks = Task.objects.filter(status=Task.ADD)
+    for i in Tasks:
+        out["Задачи"].append({
+            "Имя" : i.title,
+            "Описание" : i.description,
+            "Состояние": i.status.title(),
+            "id": i.id,
+            "Время" : i.create_dateTime,
+        })
+    Tasks = Task.objects.filter(status=Task.END)
     for i in Tasks:
         out["Задачи"].append({
             "Имя" : i.title,
