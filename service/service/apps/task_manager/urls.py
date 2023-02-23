c011f18e17f8f9afbe4e75864dcc0d4de1b74551
superdef454 <superdef454@mail.ru>
2023-02-23 12:08:11 +0500

    половина функционала кнопок

--- service/service/apps/task_manager/urls.py
+++ service/service/apps/task_manager/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.tasks, name = "tasks"),
+    path('<int:id>/<str:status>', views.tasks, name="change")
 ]
--- service/service/apps/task_manager/views.py
+++ service/service/apps/task_manager/views.py
@@ -2,50 +2,33 @@ from django.shortcuts import render
 
 from .models import Task
 
-def tasks(request):
-    # print(request.GET.get("Title"), request.GET.get("description"), request.GET.get("status"))
-    # Tasks = Task.objects.all()
-    # out = {"Задачи" : []}
-    # for i in Tasks:
-    #     out["Задачи"].append({
-    #         "Имя" : i.title,
-    #         "Описание" : i.description,
-    #         "Состояние": i.status.title(),
-    #         "Время" : "22.22.22" #i.create_dateTime.date().strftime()
-    #     })
-    out = {
-        'Задачи' : [
-            {
-                "Имя" : "Тест",
-                "Описание" : "Тестовое описание",
-                "Состояние" : "В работе",
-                "Время" : "22.12.23"
-            },
-            {
-                "Имя" : "Тест 2",
-                "Описание" : "Тестовое описание",
-                "Состояние" : "Добавлена",
-                "Время" : "22.12.23"
-            },
-            {
-                "Имя" : "Тест 3",
-                "Описание" : "Тестовое описание",
-                "Состояние" : "Выполнена",
-                "Время" : "22.12.23"
-            },
-            {
-                "Имя" : "Тест",
-                "Описание" : "Тестовое описание",
-                "Состояние" : "В работе",
-                "Время" : "22.12.23"
-            },
-            {
-                "Имя" : "Тест 2",
-                "Описание" : "Тестовое описание",
-                "Состояние" : "Добавлена",
-                "Время" : "22.12.23"
-            },
-            ],
-    }
-    print(*out["Задачи"])
+def tasks(request, id: int = -1, status: str = None):
+    if id >= 0 and status:
+        print(f"Изменить значение записи с id {id} на {status}")
+    if request.GET.get("Title") and request.GET.get("description") and request.GET.get("status"):
+        if not Task.objects.filter(title=request.GET.get("Title")).exists():
+            Task.objects.create(title=request.GET.get("Title"), description = request.GET.get("description"), status = request.GET.get("status"))
+    Tasks = Task.objects.all()
+    out = {"Задачи" : []}
+    for i in Tasks:
+        out["Задачи"].append({
+            "Имя" : i.title,
+            "Описание" : i.description,
+            "Состояние": i.status.title(),
+            "id": i.id,
+            "Время" : i.create_dateTime,
+        })
+    return render(request, 'index.html', out)
+
+def changeStatus(request):
+    Tasks = Task.objects.all()
+    out = {"Задачи" : []}
+    for i in Tasks:
+        out["Задачи"].append({
+            "Имя" : i.title,
+            "Описание" : i.description,
+            "Состояние": i.status.title(),
+            "id": i.id,
+            "Время" : i.create_dateTime,
+        })
     return render(request, 'index.html', out)
